8913889d939f7fb210ec2d1c2e95549c5180ae8b
caoquangkien <caoquangkien@gmail.com>
2025-08-03 22:31:59 +0700

    modified:   numpyarray.py

--- numpyarray.py
+++ numpyarray.py
@@ -1,12 +1,8 @@
 import numpy as np
 
 arr = np.array ([1,2,3,4,5])
-print (arr)
-
-print (arr[0])
-
-arr[0] =200
-print (arr)
 
 x = arr.copy()
-print (x)
+y = arr.view()
+print (x.base)
+print (y.base)
